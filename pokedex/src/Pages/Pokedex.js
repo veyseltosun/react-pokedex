a2d3464ba8649990147b3bb54b1cb9cf0bcee4fc
veyseltosun <postalarim1923@gmail.com>
2023-01-11 13:55:43 +0300

    keep going building up the app.

--- pokedex/src/Pages/Pokedex.js
+++ pokedex/src/Pages/Pokedex.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import axios from "axios";
+import { IMAGE_API_URL, POKEMON_API_URL } from '../config';
+import { CircularProgress } from '@mui/material';
+import { Box } from '@mui/system';
 
 
 
 const Pokedex = () => {
+    const[pokemonData, setPokemonData] = React.useState(null)
+    useEffect(() => {
+
+        axios.get(POKEMON_API_URL + "?limit=800").then((response) => {
+            if(response.status >= 200 && response.status < 300) {
+                const {results} = response.data;
+                let newPokemonData = [] ;
+                results.forEach((pokemon, index) => {
+               
+                        let pokemonObject = {
+                            id: index,
+                            url: IMAGE_API_URL+index+".png",
+                            name: pokemon.name
+                        }
+                        newPokemonData.push(pokemonObject)
+                    
+                });
+                setPokemonData(newPokemonData)
+            }
+        })
+
+    }, [])
   return (
-    <div>Pokedex</div>
+    <Box>
+        {pokemonData ? pokemonData.map((pokemon) => {
+            return (
+                <h1 key={pokemon.id}>{pokemon.name}</h1>
+            )
+        }) : <CircularProgress sx={{marginTop:100}} />}
+    </Box>
   )
 }
 
--- pokedex/src/components/Navbar.jsx
+++ pokedex/src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{backgroundColor:"black",  }}>
+        <Toolbar >
+         <Link to="/" style={{textDecoration: "none", color:"white"}} >
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1,}}>
+            Pokedex
+          </Typography>
+          </Link>
+          
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default Navbar
