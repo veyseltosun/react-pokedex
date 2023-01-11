2e51b3d6e25caf7b09595a3d56104f7c26c2c9a9
veyseltosun <postalarim1923@gmail.com>
2023-01-11 12:50:03 +0300

    getting start again.

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import Navbar from './components/Navbar'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Pokedex from './Pages/Pokedex';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Pokedex/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
